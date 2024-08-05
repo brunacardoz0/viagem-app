925e38e37e64c0c436b28cbf6df1c9c09accebf5
BRUNA HUGO CARDOZO <bruna_h_cardozo@estudante.sesisenai.org.br>
2024-08-05 09:24:57 -0300

    Estrutura Básica e Componentes

---
+++ src/Header.jsx
@@ -0,0 +1,37 @@
+#jsx
+
+// Header.jsx
+import React from 'react';
+
+const Header = () => {
+  return <header><h1>Aplicativo de Viagens</h1></header>;
+};
+
+export default Header;
+
+// Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return <footer><p>
+ 2024 Viagem App</p></footer>;
+};
+
+export default Footer;
+
+// App.jsx
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Outros componentes virão aqui */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
